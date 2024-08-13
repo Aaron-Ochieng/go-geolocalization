6c109d9fe07a427c1658eb1ee5580bfdf29dbdfb
moonyango <mosesadrian825@gmail.com>
2024-08-13 18:51:33 +0300

    fix: print to see the output

--- main.go
+++ main.go
@@ -9,5 +9,11 @@ func main() {
 	apiIndex := utils.GetApiIndex()
 
 	Artists := utils.GetArtists(apiIndex.Artists)
+	Locations, err := utils.GetLocations(apiIndex.Locations)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	fmt.Println(Artists[0].CreationDate)
+	fmt.Println(Locations[0])
 }
