6336a8fcfa14b5f1bb5e7e5c4f6b4c92106a2489
moonyango <mosesadrian825@gmail.com>
2024-08-13 17:59:51 +0300

    fix: print result from getArtists to see the output

--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 func main() {
 	apiIndex := utils.GetApiIndex()
 
-	fmt.Printf("Artists API: %s\n", apiIndex.Artists)
-	fmt.Printf("Locations API: %s\n", apiIndex.Locations)
-	fmt.Printf("Dates API: %s\n", apiIndex.Dates)
-	fmt.Printf("Relation API: %s\n", apiIndex.Relation)
+	Artists := utils.GetArtists(apiIndex.Artists)
+	fmt.Println(Artists[0].CreationDate)
 }
