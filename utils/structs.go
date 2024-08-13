a4d509a2d8810cdff42070b1660f47a87055f9b9
moonyango <mosesadrian825@gmail.com>
2024-08-13 17:58:04 +0300

    feat: add struct for Artist

--- utils/structs.go
+++ utils/structs.go
@@ -6,3 +6,15 @@ type ApiIndex struct {
 	Dates     string `json:"dates"`
 	Relation  string `json:"relation"`
 }
+
+type Artist struct {
+	Id           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
