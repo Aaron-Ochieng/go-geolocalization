6b0ad34b2a9e62be1ee54d931d8f85024bf02645
Aaron Ochieng <ochiengaoron@gmail.com>
2024-08-22 11:38:19 +0300

    Fix: Add username to the locations slice and fixed internal server error

--- handlers/locationshandler.go
+++ handlers/locationshandler.go
@@ -1,9 +1,10 @@
 package handlers
 
 import (
-	"groupie-tracker/utils"
 	"net/http"
 	"text/template"
+
+	"groupie-tracker/utils"
 )
 
 func LocationsHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,10 +13,20 @@ func LocationsHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error fetching locations", http.StatusInternalServerError)
 		return
 	}
+	usernames, err := utils.GetArtists()
+	if err != nil {
+		http.Error(w, "Error fetching locations", http.StatusInternalServerError)
+		return
+	}
 	tmpl, err := template.ParseFiles("templates/locations.html")
 	if err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
+	}
+
+	for i, val := range usernames {
+		locations[i].Name = val.Name
+		// fmt.Println(val.Name)
 	}
 	tmpl.Execute(w, locations)
 }
